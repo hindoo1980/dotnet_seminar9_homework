f3dda32acf4fbb27d2fc2f2f556702b52a145f97
DENIS <hindoo@mail.ru>
2022-12-03 00:57:55 +0300

    clear third exercises

--- exercises/seminar9_68/Program.cs
+++ exercises/seminar9_68/Program.cs
@@ -1,36 +1,25 @@
 ﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 
-// static class GlobalVars
-// {
-//     public static uint a;
-// }
-
-class Akkerman
-{
-     public static int akk(int n, int m) 
+    int akk(int n, int m) 
     {
         if (n == 0) 
         {
-            //GlobalVars.a++;
             return  m + 1 ;
         } 
         else 
         {          
             if (n != 0 && m == 0)
             {
-                //GlobalVars.a++;
                 return akk(n - 1, 1);              
             }     
             else
             {
-                //GlobalVars.a++;
                 return akk(n - 1 , akk(n, m - 1));              
             }
         }
     }
   
-  static void Main() 
-  {
+
 
      string tmp = string.Empty;
 
@@ -59,7 +48,5 @@ class Akkerman
         }
 
     Console.WriteLine(); Console.WriteLine ($"результат: {akk(n, m)}");
-    //Console.WriteLine (GlobalVars.a);
-  }
+
   
-}
