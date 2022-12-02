2a9e6155aa1246b96bfe496d7482fbf6ebd731b7
DENIS <hindoo@mail.ru>
2022-12-03 00:29:57 +0300

    1+3

---
+++ exercises/seminar9_64/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+    void recursion(int n) 
+    {
+        if (n == 1) 
+        {
+           Console.WriteLine(1);
+        } 
+        else 
+        {   
+            Console.WriteLine(n);
+            recursion(n - 1);
+        }
+    }
+  
+    string tmp = string.Empty;
+
+    Console.Write($"введите целым положительным числом значение N: ");
+
+    tmp =  Console.ReadLine();
+
+    bool check = int.TryParse(tmp, out int n);
+
+    if (check == false || n < 0 )
+    {
+        Console.WriteLine("неверный ввод");
+        Environment.Exit(0);
+    }
+
+    recursion(n);
---
+++ exercises/seminar9_68/Program.cs
@@ -0,0 +1,65 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+// static class GlobalVars
+// {
+//     public static uint a;
+// }
+
+class Akkerman
+{
+     public static int akk(int n, int m) 
+    {
+        if (n == 0) 
+        {
+            //GlobalVars.a++;
+            return  m + 1 ;
+        } 
+        else 
+        {          
+            if (n != 0 && m == 0)
+            {
+                //GlobalVars.a++;
+                return akk(n - 1, 1);              
+            }     
+            else
+            {
+                //GlobalVars.a++;
+                return akk(n - 1 , akk(n, m - 1));              
+            }
+        }
+    }
+  
+  static void Main() 
+  {
+
+     string tmp = string.Empty;
+
+        Console.WriteLine($"введите целым положительным числом значение n:");
+
+        tmp =  Console.ReadLine();
+
+        bool check = int.TryParse(tmp, out int n);
+
+        if (check == false || n < 0 )
+        {
+            Console.WriteLine("неверный ввод");
+            Environment.Exit(0);
+        }
+
+        Console.WriteLine($"введите целым положительным числом значение m:");
+
+        tmp =  Console.ReadLine();
+
+        check = int.TryParse(tmp, out int m );
+
+        if (check == false || m < 0 )
+        {
+            Console.WriteLine("неверный ввод");
+            Environment.Exit(0);
+        }
+
+    Console.WriteLine(); Console.WriteLine ($"результат: {akk(n, m)}");
+    //Console.WriteLine (GlobalVars.a);
+  }
+  
+}
